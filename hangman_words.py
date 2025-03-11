5fccf6165bfb6fb1984c4e42d46e0a81a93a9ffd
f-zafar <fatimazafar2005@gmail.com>
2025-03-11 05:25:03 +0100

    Inital commit

---
+++ hangman_words.py
@@ -0,0 +1,12 @@
+words = [
+    "alligator", "angry", "apple", "autumn", "backpack", "banana", "basketball", "bed", "blueberry", "bored",
+    "burger", "calm", "calculator", "carpet", "chair", "cheetah", "cherry", "cloud", "confused", "crocodile",
+    "curtain", "cycling", "desk", "desert", "django", "dolphin", "eraser", "excited", "flamingo", "forest",
+    "fries", "giraffe", "golf", "grape", "happy", "highlighter", "html", "island", "java", "javascript",
+    "kangaroo", "kiwi", "lamp", "leopard", "marker", "mirror", "mountain", "nervous", "node", "notebook",
+    "ocean", "omelet", "orange", "pancake", "paper", "past", "peach", "pen", "pencil", "penguin",
+    "pizza", "python", "rain", "react", "river", "ruler", "rugby", "sad", "salad", "sandwich",
+    "scared", "shelf", "snow", "soccer", "sofa", "spring", "steak", "storm", "strawberry", "summer",
+    "sunshine", "surprised", "swimming", "table", "tennis", "valley", "volcano", "volleyball", "waterfall", "watermelon",
+    "wind", "winter"
+]
